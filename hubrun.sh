7f659227f62f3be7add5cdb2c58af8260f921bfb
davevs <dvanstein@qxperts.io>
2022-03-13 14:44:52 +0100

    bugfixes

---
+++ hubrun.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker rm dvxte-hub
+docker rmi dvxte-hub
+docker run --name dvxte-hub -p 80:80 -p 1080:1080 -p 3000:3000 -p 4000:4000 -p 5013:5013 -p 8000:8000 -p 8200:8200 -p 8300:8300 -p 8400:8400 -p 9000:9000 dvsteinqx/dvxte:0.2
